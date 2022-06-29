e7465da943dbfb46b409a6b96a9df2eaa3b549ed
homasha <homasha@gmail.com>
2022-06-29 19:52:35 +0200

    Search engine ver1.1. Adding search for user names by any letter.

--- search/search.php
+++ search/search.php
@@ -1,10 +1,24 @@
 <?php
-  if (session_status() == SESSION_PHP_NONE)
+  if (session_status() == PHP_SESSION_NONE)
   {
     session_start();
   }
-
   require_once('../config/connection.php');
 
-  echo 'SEARCH';
+  $search = $_POST['search'];
+  // echo $search;
+
+  $query = "SELECT first_name FROM customer WHERE first_name LIKE '%".$search."%'";
+  $result = $connection->query($query);
+  if (mysqli_num_rows($result) == 0)
+  {
+    echo 'NIE';
+  }
+  else
+  {
+    while ($row = $result->fetch_assoc())
+    {
+      echo $row['first_name'].'<br>';
+    }
+  }
 ?>
