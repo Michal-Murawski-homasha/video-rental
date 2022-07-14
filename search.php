3f711eb0c7bb4140c0bc48fa37e57a6043073af2
Michał Murawski <40899010+homasha@users.noreply.github.com>
2022-07-14 22:01:10 +0200

    Move the $row variable directly into the tbody tag.

--- search.php
+++ search.php
@@ -155,20 +155,20 @@
 
                                                     while ($row = $result->fetch())
                                                     {
-                                                      // $filmId = $row['film_id'];
-                                                      // $title = $row['title'];
-                                                      // $releaseYear = $row['release_year'];
-                                                      // $name = $row['name'];
-                                                      // $length = $row['length'];
-                                                      // $rentalRate = $row['rental_rate'];
+                                                      $filmId = $row['film_id'];
+                                                      $title = $row['title'];
+                                                      $releaseYear = $row['release_year'];
+                                                      $name = $row['name'];
+                                                      $length = $row['length'];
+                                                      $rentalRate = $row['rental_rate'];
 
                                                       echo '<tbody>
-                                                      <td>'.$row['filmId'].'</td>
-                                                      <td>'.$row['title'].'</td>
-                                                      <td>'.$row['releaseYear'].'</td>
-                                                      <td>'.$row['name'].'</td>
-                                                      <td>'.$row['length'].'</td>
-                                                      <td>'.$row['rentalRate'].'</td>
+                                                      <td>'.$filmId.'</td>
+                                                      <td>'.$title.'</td>
+                                                      <td>'.$releaseYear.'</td>
+                                                      <td>'.$name.'</td>
+                                                      <td>'.$length.'</td>
+                                                      <td>'.$rentalRate.'</td>
                                                       </tbody>';
                                                     }
                                                     echo  '</table>';
